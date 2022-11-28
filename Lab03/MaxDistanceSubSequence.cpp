bc4cbbc82d4f3f841d8885cb9d9068fcc9e9f6fc
Cmint22 <you@example.com>
2022-11-28 13:04:12 +0700

    first commit

---
+++ Lab03/MaxDistanceSubSequence.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int maxn = 100001;
+int n, c, x[maxn];
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin >> n >> c;
+        memset(x, 0, sizeof(x));
+        for(int i = 0; i < n; ++i) {
+            cin >> x[i];
+        }
+    }
+}
